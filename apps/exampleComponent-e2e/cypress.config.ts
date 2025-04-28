020c4e4aff565239c5d19fb4e2c255975cbcc6fd
Cesar Merlo <julio.cmb@rytsolucion.com>
2025-04-27 23:15:33 -0600

    Initial commit 🚀

---
+++ apps/exampleComponent-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run exampleComponent:serve',
+        production: 'npx nx run exampleComponent:preview',
+      },
+      ciWebServerCommand: 'npx nx run exampleComponent:preview',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4201',
+  },
+});
---
+++ apps/exampleComponent/src/app/app.tsx
@@ -0,0 +1,12 @@
+import NxWelcome from './nx-welcome';
+import '../styles.css';
+
+export function App() {
+  return (
+    <div className='bg-slate-300'>
+      <NxWelcome title="exampleComponent" />
+    </div>
+  );
+}
+
+export default App;
